90d190fabaa4676cb43b9c71cb2bba35a5653963
JuliaMalmliden <50831294+JuliaMalmliden@users.noreply.github.com>
2020-10-20 17:43:08 +0200

    produktutveckling hehehe

--- WarehouseEN1/Product.cs
+++ WarehouseEN1/Product.cs
@@ -8,9 +8,37 @@ namespace WarehouseEN1
     {
         private int productID;
         private string productName;
-        private int productPrice;
-        private int productStock; 
-        private 
+        private double productPrice;
+        private int productStock;
+        private DateTime nextRestock; 
+        public int ProductID { get { return productID; } set { productID = value; } }
+        public string ProductName { get { return productName; } set { productName = value; } }
+        public double ProductPrice { get { return productPrice; } set { productPrice = value; } }
+        public int ProductStock { get { return productStock; } set { productStock = value; } }
+        public DateTime NextRestock { get { return nextRestock; } set { nextRestock = value; } }
+
+
+        public Product()
+        {
+
+        }
+        public Product(int id, string nm, double pr, int stk, DateTime ns)
+        {
+            ProductID = id; 
+            ProductName = nm;
+            ProductPrice = pr;
+            ProductStock = stk;
+            NextRestock = ns; 
+
+        }
+        public override string ToString()
+        {
+            return ProductName + "(" + ProductPrice + ")";
+        }
+        public bool PriceIsValid()
+        {
+            return ProductPrice >= 0 && ProductPrice <= 9999;
+        }
 
 
 
